f8b93ff409be06dfbfa6663e6befba060c6a2a2f
ONEO <oneo.me@outlook.com>
2020-08-04 11:34:21 +0800

    兼容 .Net Core

--- ExFat.Core/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// This is ExFat, an exFAT accessor written in pure C#
-// Released under MIT license
-// https://github.com/picrap/ExFat
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("ExFat Core")]
-[assembly: AssemblyDescription("exFAT core module for basic I/O")]
-
--- ExFat.Generator/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// This is ExFat, an exFAT accessor written in pure C#
-// Released under MIT license
-// https://github.com/picrap/ExFat
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("ExFat generator")]
-[assembly: AssemblyDescription("Internal application to generate test disk")]
-
